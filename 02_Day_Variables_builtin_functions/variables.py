e2862e76023a3ee5acefd5254656b0c74c098093
Joany Hariliva <joany1043@gmail.com>
2026-04-18 14:49:43 +0300

    Refactor variables and add circle calculations
    Refactor variables and add calculations for circle area and circumference. Include user input for radius and print formatted output.

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,68 @@
+#Jour 2 : 30 jours de programmation Python 
+first_name="Joany Hariliva"
+name_family="NJATOVOMANANA"
+comple_name="NJATOVOMANANA Joany Hariliva"
+country="Madagascar"
+city="Antananarivo"
+age=23
+years=2026
+is_married=False
+is_true=True
+is_light_on=True
+name,first_name,age,country="NJATOVOMANANA","Joany Hariliva",23,"Madagascar"
+#Niveau2
+print(type(first_name))
+print(type(name_family))
+print(type(comple_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(years))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
 
-# Variables in Python
+len_first_name=len(first_name))
+len_name_family=len(name_family))
+print(len_first_name)
+print(len_name_family)
+if len_first_name>len_name_family:
+    print("first name est plus longue que le nom de family")
+elif len_first_name<len_name_family:
+    print("first name est plus court que le nom de family")
+else:
+    print("La longeur de de name est first name est equals")
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname': 'Asabeneh',
-    'lastname': 'Yetayeh',
-    'country': 'Finland',
-    'city': 'Helsinki'
-}
+#Declaration de variable 5 et 4
+num_one=5
+num_two=4
+var_total=num_one+num_two
+var_diff=num_two-num_one
+var_produit=num_two*num_one
+var_div=num_one/num_two
+var_ram=num_two%num_one
+var_exp=num_one**num_two
+floor_division=num_one//num_two
 
-# Printing the values stored in the variables
+#12 RAYON D'UN CERCLE
+rayon_cercle=30
+aire_du_cercle=3.14*rayon_cercle**2
+circum_of_circle=2*3.14*rayon_cercle
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+rayon_cercle=int(input("Entrer le rayon: "))
+aire_du_cercle=3.14*rayon_cercle**2
+circum_of_circle=2*3.14*rayon_cercle
+print(aire_du_cercle)
+print(circum_of_circle)
 
-# Declaring multiple variables in one line
+#Fonction saisie integrer
+first_name=str(input("First name: "))
+name_family=str(input("Family name: "))
+country=str(input("Your contry: "))
+age=int(input("Your age: "))
+print(f"Tu es {first_name} Ton nom de famille est {name_family} tu habite a {country} et tu as {age}")
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+
+
+
